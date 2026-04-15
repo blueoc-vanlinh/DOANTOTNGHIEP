8f4907a79680cce651ddff18cbde49d628cf0b1c
blueoc-vanlinh <linh.nguyenvan@blue.tech>
2026-04-16 02:55:46 +0700

    fix

--- frontend/src/constants/config.ts
+++ frontend/src/constants/config.ts
@@ -1,1 +1,3 @@
-export const BASE_URL = import.meta.env.VITE_BASE_URL;
+export const BASE_URL =
+    import.meta.env.VITE_BASE_URL ||
+    `${window.location.protocol}//${window.location.hostname}:8000/api/v1`;
