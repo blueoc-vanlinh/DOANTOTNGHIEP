6460102e526c0b42f2bd2e8a3b6e69d304b16ac4
blueoc-vanlinh <linh.nguyenvan@blue.tech>
2026-04-17 02:16:31 +0700

    fix ip backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,10 @@ app = FastAPI()
 app.include_router(api_router, prefix="/api/v1")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", 
+                   "http://127.0.0.1:5173",
+                   "http://192.168.1.50:5173",
+                   "http://192.168.1.50:5174"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
