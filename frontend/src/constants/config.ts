672ed27e6ff4da62395e5f32f3ec65ed5702dc08
blueoc-vanlinh <linh.nguyenvan@blue.tech>
2026-04-17 18:24:25 +0700

    fix ip backend

--- frontend/src/constants/config.ts
+++ frontend/src/constants/config.ts
@@ -1,3 +1,1 @@
-export const BASE_URL =
-    import.meta.env.VITE_BASE_URL ||
-    `${window.location.protocol}//${window.location.hostname}:8000/api/v1`;
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
