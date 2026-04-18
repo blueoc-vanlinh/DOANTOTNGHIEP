accb514d7db5a14bcfc50ebc70de81bbbb3b212d
blueoc-vanlinh <linh.nguyenvan@blue.tech>
2026-04-18 23:31:16 +0700

    checkenv

--- frontend/src/constants/config.ts
+++ frontend/src/constants/config.ts
@@ -1,1 +1,10 @@
-export const BASE_URL = import.meta.env.VITE_API_URL;
+const portMap: Record<string, string> = {
+    "5173": "8000",
+    "5174": "8001",
+    "5175": "8002",
+};
+
+const backendPort = portMap[window.location.port] || "8000";
+
+export const BASE_URL =
+    `${window.location.protocol}//${window.location.hostname}:${backendPort}/api/v1`;
